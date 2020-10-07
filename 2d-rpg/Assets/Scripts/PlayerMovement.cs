40a8d6d5361a10e1b23675da4cd9e8fc660676ce
Matías Rivera <juanmatiasrivera@outlook.com>
2020-10-07 07:38:03 -0300

    Added Breakables Pots and State Machine for Player

--- 2d-rpg/Assets/Scripts/PlayerMovement.cs
+++ 2d-rpg/Assets/Scripts/PlayerMovement.cs
@@ -2,9 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
 
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
@@ -13,8 +22,11 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -23,18 +35,22 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        if (Input.GetButtonDown("Attack")){
+        if (Input.GetButtonDown("Attack") && currentState != PlayerState.attack){
             StartCoroutine(AttackCo());
+        }else if (currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
         }
-        UpdateAnimationAndMove();
     }
 
     private IEnumerator AttackCo()
     {
         animator.SetBool("attacking", true);
+        currentState = PlayerState.attack;
         yield return null;
         animator.SetBool("attacking",false);
         yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
     }
 
     private void UpdateAnimationAndMove()
@@ -55,6 +71,7 @@ public class PlayerMovement : MonoBehaviour
 
     void MoveCharacter()
     {
+        change.Normalize();
         myRigidbody.MovePosition(
             transform.position + change * speed * Time.deltaTime
         );
